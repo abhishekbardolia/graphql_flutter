1e807d42d8a722a3b16c5f4391b996ec461c6d9d
abhishekbardolia <abhibardolia@gmail.com>
2022-08-06 11:38:58 +0530

    removed unneccessary comment

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,6 @@ class _MyHomePageState extends State<MyHomePage> {
               );
             }),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -105,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     setState((){
-      character = queryResult.data!['characters']['results']; // taking in the character list now we will use this data to display
+      character = queryResult.data!['characters']['results'];
       _loading = false;
     });
   }
