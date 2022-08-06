67d92e35f24a1abe804d51dd425449a3cbe8ccfc
abhishekbardolia <abhibardolia@gmail.com>
2022-08-06 11:29:10 +0530

    All comments had been removed.

--- lib/main.dart
+++ lib/main.dart
@@ -36,14 +36,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,11 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
   List<dynamic> character = [];
   bool _loading = false;
 
-  // Why to use graphql?
-  // It is faster than efficient than rest
-  // You can get that field only which is required, remove those which you don't want
-  // Here we are using
-  // https://rickandmortyapi.com/graphql
 
   @override
   Widget build(BuildContext context) {
@@ -116,11 +103,6 @@ class _MyHomePageState extends State<MyHomePage> {
 }
         """))
     );
-
-    // queryResult.data // It contains data
-    // queryResult.exception // will give you exception
-    // queryResult.hasException //you can check if you have any exception or not
-    // queryResult.context.entry<HttpLinkResponseContext>()?.statusCode // to get status code of response
 
     setState((){
       character = queryResult.data!['characters']['results']; // taking in the character list now we will use this data to display
